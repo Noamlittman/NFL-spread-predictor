e4926ac0a776fd47297372e74a4e91a0da4206c5
Administrator <nlittman@gmail.com>
2023-12-12 18:22:32 +0200

    second code upload

--- Collect_all_the_data.py
+++ Collect_all_the_data.py
@@ -1,6 +1,7 @@
 
 from Collect_game_stats_and_personnel import *
-
+from Colle import *
+from Collect_Betting_Odds import *
 def Collect_the_data:
     team_ovr_df_years, player_team_stats_madden_years_1 = take_madden_stats(start, end, file_path)
     madden_data = player_team_stats_madden_years_1
