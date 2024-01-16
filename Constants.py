1a48570696db3bcf911e5422cce17f4f614a19dc
Administrator <nlittman@gmail.com>
2024-01-16 10:44:48 +0200

    Updates

--- Constants.py
+++ Constants.py
@@ -2,8 +2,8 @@ start = 2009
 end = 2024
 current_week = 13
 current_year = 2024
-desired week = 14
-desired year = 2024
+desired_week = 14
+desired_year = 2024
 file_path = "C:\\Users\\Administrator\\Desktop\\NFL Project"
 
 teams_file_path = 'C:\\Users\\Administrator\\Desktop\\NFL Project\\team names.xlsx'
